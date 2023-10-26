62e31f1db1f1bade3d8ff96a433f00b2d141f710
Rahmad Alfian Syahputra <rahmadalfiansyahputra48@gmail.com>
2023-10-26 15:04:48 +0700

    Digital Signage Web First Release

---
+++ components/SliderComponents.tsx
@@ -0,0 +1,65 @@
+"use client";
+// "use server";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ReactPlayer from "react-player";
+
+interface DataItem {
+  id: number;
+  type: string;
+  videoId: string;
+  image: string | null;
+  status: string;
+  urutan: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+const SliderComponents = () => {
+  const [data, setData] = useState<DataItem[]>([]);
+  const [videoPlayer, setVideoPlayer] = useState("kdklv9Vvjpg");
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    axios.get("https://langsacarong.langsakota.go.id/api/ds").then((res) => {
+      const responseData: DataItem[] = res.data.data;
+      setData(responseData);
+      const filteredData = responseData.filter((item) => item.type === "video");
+      const minUrutan = Math.min(...filteredData.map((item) => item.urutan));
+      const result = filteredData.filter((item) => item.urutan === minUrutan);
+      setVideoPlayer(result[counter].videoId);
+    });
+  }, []);
+
+  const refreshDataAndVideo = () => {
+    axios.get("https://langsacarong.langsakota.go.id/api/ds").then((res) => {
+      const responseData: DataItem[] = res.data.data;
+      setData(responseData);
+      console.log(responseData);
+    });
+  }
+
+
+  return (
+    <ReactPlayer
+      url={`https://www.youtube.com/embed/${videoPlayer}?autoplay=1&controls=0`}
+      controls={false}
+      width={"100%"}
+      height={"100%"}
+      playing={true}
+      onEnded={() => {
+        // console.log("ended video");
+        if (counter === data.length - 1) {
+          setVideoPlayer(data[0].videoId);
+          setCounter(0);
+          return;
+        }
+        refreshDataAndVideo();
+        setVideoPlayer(data[counter + 1].videoId);
+        setCounter(counter + 1);
+      }}
+    />
+  );
+};
+
+export default SliderComponents;
