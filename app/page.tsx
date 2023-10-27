8e82d70dca1624c6fce85ada012107cb5366b467
Rahmad Alfian Syahputra <rahmadalfiansyahputra48@gmail.com>
2023-10-27 11:05:14 +0700

    Update 2.0

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,16 @@ import SliderComponents from "@/components/SliderComponents";
 const Home = () => {
   return (
     <div className="bg-slate-950 h-screen">
-      <SliderComponents />
+      <div className="bg-slate-950 h-1/3">
+        <SliderComponents />
+      </div>
+      <div className="bg-slate-950 h-2/3">
+        <iframe
+          src="https://dashboard.langsakota.go.id"
+          width="100%"
+          height="100%"
+        ></iframe>
+      </div>
     </div>
   );
 };
--- components/SliderComponents.tsx
+++ components/SliderComponents.tsx
@@ -43,7 +43,7 @@ const SliderComponents = () => {
   return (
     <ReactPlayer
       url={`https://www.youtube.com/embed/${videoPlayer}?autoplay=1&controls=0`}
-      controls={false}
+      controls={true}
       width={"100%"}
       height={"100%"}
       playing={true}
